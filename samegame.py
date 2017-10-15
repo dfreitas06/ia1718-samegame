465d6a5137768fb31bebdf621c909300cd506932
Diogo Freitas <diogo.maria.freitas@gmail.com>
2017-10-15 13:29:11 +0100

    Environment setup
    -Implemented statement provided code;
    -Changed search.py and utils.py location;

---
+++ samegame.py
@@ -0,0 +1,65 @@
+#Same Game
+#IA 17/18
+#
+#Author: Diogo Freitas   ist181586
+#Author: Joao Rodrigues  isti83483
+
+import * from search.py
+import * from utils.py
+
+#Color
+
+def get_no_color():
+	return 0
+
+def no_color(c):
+	return c == 0
+
+def color(c):
+	return c > 0
+
+#Position
+
+def make_pos(l, c):
+	return (l, c)
+
+def pos_l(pos):
+	return pos[0]
+
+def pos_c(pos):
+	return pos[1]
+
+#Group
+
+
+#Board
+
+def board_find_groups(board):
+	return null
+
+def board_remove_group(board, group):
+	return null
+
+#SG_State
+
+#Same Game
+
+class same_game(Problem):
+
+	def _init_(self, board):
+		return null
+
+	def actions(self, state):
+		return null
+
+	def result(self, state, action):
+		return null
+
+	def goal_test(self state):
+		return null
+
+	def path_cost(self, c, state1, action, state2):
+		return null
+
+	def h(self, node):
+		return null
